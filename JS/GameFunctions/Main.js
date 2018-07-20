5052d5997dda7f4938bec6bbf529d203f5f7ab56
Server <ahmedkhaled36@hotmail.com>
2018-07-20 23:33:15 +0200

    Responsive game View

--- JS/GameFunctions/Main.js
+++ JS/GameFunctions/Main.js
@@ -11,9 +11,35 @@ function init() {
     geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
     material = new THREE.MeshNormalMaterial();
 
+    // material.wireframe = true;
+
+
     mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
 
+
+    //This will add a starfield to the background of a scene
+var starsGeometry = new THREE.Geometry();
+
+for ( var i = 0; i < 100 ; i ++ ) {
+
+	var star = new THREE.Vector3();
+	star.y = THREE.Math.randFloatSpread( 5 );
+	star.z = THREE.Math.randFloatSpread( 4 );
+    star.x = THREE.Math.randFloatSpread( 3 );
+
+    star.velocity = 12;
+
+	starsGeometry.vertices.push( star );
+}
+
+var starsMaterial = new THREE.PointsMaterial( { color: 0xFFFFFF , size : 0.0001 } );
+
+var starField = new THREE.Points( starsGeometry, starsMaterial );
+
+scene.add( starField );
+
+    
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
@@ -24,8 +50,8 @@ function animate() {
 
     requestAnimationFrame(animate);
 
-    mesh.rotation.x += 0.05;
-    mesh.rotation.y += 0.09;
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    mesh.rotation.x += 0.01;
 
     renderer.render(scene, camera);
     $('#UserName').find("h1").text("Li");
